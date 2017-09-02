9eebce0fe7df7ca8fce8ecd5dcc9353e54b9f5e4
whytellyou <likev@qq.com>
2017-09-02 23:21:28 +0800

    add .hpp

---
+++ use-cimg/CImg/classCImg02.hpp
@@ -0,0 +1,20 @@
+#ifndef _CLASS_CIMG_HPP_02
+#define _CLASS_CIMG_HPP_02
+
+
+#include "classCImg03.h"
+
+namespace cimg_library_suffixed {
+
+template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::
+template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::
+template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::
+template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::
+template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::
+template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::
+template<typename T>  CImg<T>::  template<typename T>  CImg<T>::  template<typename T>  CImg<T>::
+
+
+}//cimg_library_suffixed
+
+#endif // !_CLASS_CIMG_HPP
